1db12b3f546d274efda6af711aba26cf87780576
Jihee <sjh709@naver.com>
2024-05-28 07:13:16 +0900

    기능 구현

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,20 +1,13 @@
 const express = require('express');
+const taskController = require('../controller/task.controller');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  res.send('create task');
-});
+router.post('/', taskController.createTask);
 
-router.get('/', (req, res) => {
-  res.send('get tasks');
-});
+router.get('/', taskController.getTask);
 
-router.put('/:id', (req, res) => {
-  res.send('update task');
-});
+router.put('/:id', taskController.updateTask);
 
-router.delete('/:id', (req, res) => {
-  res.send('delete task');
-});
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
