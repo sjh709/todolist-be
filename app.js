322bbab0ca4650be7c475711eb1439f55fc0cf84
Jihee <sjh709@naver.com>
2024-05-28 10:50:55 +0900

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api', indexRouter);
 
 const mongoURI = `mongodb://localhost:27017/todo-demo`;
