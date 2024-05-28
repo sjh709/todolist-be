7dd52ee9e3c887b8d05138080b9a1e7b6ae8773d
Jihee <sjh709@naver.com>
2024-05-29 04:02:08 +0900

    데이터베이스 배포관련 세팅

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-
+require('dotenv').config();
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+// const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => {
